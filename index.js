a12eb6326664cc626511469ee88f35b2321fb02d
Remek-P <remek.piotrowski@wp.pl>
2023-03-22 15:10:24 +0100

    Key codes task

---
+++ index.js
@@ -0,0 +1,31 @@
+const createHTML = (obj) => {
+  const insert = document.getElementById("insert");
+  insert.innerHTML = "";
+
+  for (let key in obj) {
+    const div = document.createElement("div");
+    div.className = "key"
+    const small = document.createElement("small");
+
+    const text = document.createTextNode(key);
+    const value = document.createTextNode(obj[key]);
+
+    small.appendChild(text);
+    div.appendChild(value);
+    div.appendChild(small);
+    insert.appendChild(div);
+  }
+}
+
+const displayKey = (e) => {
+
+  const keyCodes = {
+    "key": e.key !== "Space" ? e.key : "Space",
+    "keyCode": e.keyCode,
+    "code": e.code,
+  };
+
+  createHTML(keyCodes);
+};
+
+window.addEventListener("keypress", displayKey);
